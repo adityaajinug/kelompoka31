9b0cfb015ad4bd1462dcd982f8edf9a7ee8acc85
Aditya Ajie Nugroho <adityaajinug@gmail.com>
2022-03-16 23:27:01 +0700

    manambahkan code yang kurang

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const menuToggle = document.querySelector('.menu-toggle input');
-const nav = document.querySelector('nav ul');
+const nav = document.querySelector('.navigation ul');
 
 
 menuToggle.addEventListener('click', function () {
